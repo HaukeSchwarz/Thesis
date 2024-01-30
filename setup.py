3b41861df7b058cd97b7cb6f5e1f800a282c5836
MoronLeader <121503682+MoronLeader@users.noreply.github.com>
2024-01-30 20:35:06 +0100

    Introduced cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Test',
+    author='Hauke Schwarz',
+    license='MIT',
+)
